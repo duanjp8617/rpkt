cd6c83e6758525c9bc9d77f7031ee7dd1da9099e
duanjp8617 <duanjp8617@gmail.com>
2023-11-09 09:10:56 +0000

    Finalize ipv4 option

--- run-packet/src/ipv4/option.rs
+++ run-packet/src/ipv4/option.rs
@@ -28,12 +28,6 @@ pub struct Timestamp<T> {
     buf: T,
 }
 
-impl<T> Timestamp<T> {
-    pub const TIME_STAMP_ONLY: u8 = 0;
-    pub const IPV4_ADDR_TIME_STAMP: u8 = 1;
-    pub const PRESPECIFIED_TIME_STAMP: u8 = 3;
-}
-
 impl<T: AsRef<[u8]>> Timestamp<T> {
     pub fn flg(&self) -> u8 {
         self.buf.as_ref()[3] & 0x0f
@@ -43,16 +37,18 @@ impl<T: AsRef<[u8]>> Timestamp<T> {
         self.buf.as_ref()[3] >> 4
     }
 
-    pub fn buf(&self) -> &[u8] {
-        &self.buf.as_ref()[4..usize::from(self.buf.as_ref()[1])]
+    pub fn readable(&self) -> &[u8] {
+        let readable_len = usize::from(self.buf.as_ref()[2]) - 5;
+        &self.buf.as_ref()[4..4 + readable_len]
     }
 
-    pub fn payload_len(&self) -> usize {
-        usize::from(self.buf.as_ref()[2]) - 5
+    pub fn remaining(&self) -> usize {
+        (usize::from(self.buf.as_ref()[1]) + 1) - usize::from(self.buf.as_ref()[2])
     }
 }
 
 impl<T: AsMut<[u8]> + AsRef<[u8]>> Timestamp<T> {
+    /// possible value: 0, 1, 3
     pub fn set_flg(&mut self, value: u8) {
         self.buf.as_mut()[3] = (self.buf.as_mut()[3] & 0xf0) | (value & 0x0f);
     }
@@ -61,13 +57,14 @@ impl<T: AsMut<[u8]> + AsRef<[u8]>> Timestamp<T> {
         self.buf.as_mut()[3] += 1 << 4;
     }
 
-    pub fn buf_mut(&mut self) -> &mut [u8] {
+    pub fn writable(&mut self) -> &mut [u8] {
+        let start = usize::from(self.buf.as_ref()[2]) - 1;
         let len = usize::from(self.buf.as_mut()[1]);
-        &mut self.buf.as_mut()[4..len]
+        &mut self.buf.as_mut()[start..len]
     }
 
-    pub fn inc_payload_len(&mut self, value: usize) {
-        assert!(value + self.payload_len() <= self.buf().len());
+    pub fn inc_readable_size(&mut self, value: usize) {
+        assert!(value <= self.remaining());
         self.buf.as_mut()[2] += value as u8;
     }
 }
@@ -77,23 +74,25 @@ pub struct RecordRoute<T> {
 }
 
 impl<T: AsRef<[u8]>> RecordRoute<T> {
-    pub fn buf(&self) -> &[u8] {
-        &self.buf.as_ref()[3..usize::from(self.buf.as_ref()[1])]
+    pub fn readable(&self) -> &[u8] {
+        let readable_len = usize::from(self.buf.as_ref()[2]) - 4;
+        &self.buf.as_ref()[3..3 + readable_len]
     }
 
-    pub fn payload_len(&self) -> usize {
-        usize::from(self.buf.as_ref()[2]) - 4
+    pub fn remaining(&self) -> usize {
+        (usize::from(self.buf.as_ref()[1]) + 1) - usize::from(self.buf.as_ref()[2])
     }
 }
 
 impl<T: AsMut<[u8]> + AsRef<[u8]>> RecordRoute<T> {
-    pub fn buf_mut(&mut self) -> &mut [u8] {
+    pub fn writable(&mut self) -> &mut [u8] {
+        let start = usize::from(self.buf.as_ref()[2]) - 1;
         let len = usize::from(self.buf.as_mut()[1]);
-        &mut self.buf.as_mut()[3..len]
+        &mut self.buf.as_mut()[start..len]
     }
 
-    pub fn inc_payload_len(&mut self, value: usize) {
-        assert!(value + self.payload_len() <= self.buf().len());
+    pub fn inc_readable_size(&mut self, value: usize) {
+        assert!(value <= self.remaining());
         self.buf.as_mut()[2] += value as u8;
     }
 }
@@ -143,7 +142,7 @@ impl<'a> OptionWriter<'a> {
         self.buf[0] = TIMESTAMP;
         self.buf[1] = len as u8;
         self.buf[2] = 5;
-        (&mut self.buf[4..len]).fill(0);
+        (&mut self.buf[3..len]).fill(0);
 
         let (buf, remaining) = std::mem::replace(&mut self.buf, &mut []).split_at_mut(len);
         self.buf = remaining;
@@ -175,6 +174,14 @@ impl<'a> OptionWriter<'a> {
         self.buf = remaining;
 
         RouteAlert { buf }
+    }
+
+    pub fn from_option_bytes(buf: &'a mut [u8]) -> Self {
+        Self { buf }
+    }
+
+    pub fn remaining_bytes(&self) -> usize {
+        self.buf.len()
     }
 }
 
@@ -260,7 +267,7 @@ impl<'a> Iterator for OptionIter<'a> {
 
                         #[inline]
                         fn pointer_valid(val: u8) -> bool {
-                            val >= 4 && val <= 40 && (val - 4) % 4 == 0
+                            val >= 4 && val <= 40 && val % 4 == 0
                         }
 
                         if !opt_len_valid(opt_len)
@@ -312,7 +319,7 @@ pub struct OptionIterMut<'a> {
 }
 
 impl<'a> OptionIterMut<'a> {
-    pub fn from_option_bytes(buf: &'a mut [u8]) -> OptionIterMut<'a> {
+    pub fn from_option_bytes_mut(buf: &'a mut [u8]) -> OptionIterMut<'a> {
         Self { buf, valid: true }
     }
 }
@@ -387,7 +394,7 @@ impl<'a> Iterator for OptionIterMut<'a> {
 
                         #[inline]
                         fn pointer_valid(val: u8) -> bool {
-                            val >= 4 && val <= 40 && (val - 4) % 4 == 0
+                            val >= 4 && val <= 40 && val % 4 == 0
                         }
 
                         if !opt_len_valid(opt_len)
@@ -433,6 +440,205 @@ impl<'a> Iterator for OptionIterMut<'a> {
                     }
                 }
             }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use byteorder::{ByteOrder, NetworkEndian};
+
+    #[test]
+    fn test_ipv4_option() {
+        let mut buf = [0; 200];
+
+        let mut writer = OptionWriter::from_option_bytes(&mut buf[..]);
+
+        let mut ts = writer.ts(16);
+        let mut rr = writer.rr(15);
+        writer.nop();
+        let mut ra = writer.ra();
+        writer.nop();
+        writer.nop();
+        writer.nop();
+        writer.eol();
+
+        assert_eq!(ts.flg(), 0);
+        assert_eq!(ts.overflow(), 0);
+        assert_eq!(ts.readable().len(), 0);
+        assert_eq!(ts.remaining(), 12);
+        assert_eq!(ts.writable().len(), 12);
+
+        ts.set_flg(3);
+        ts.inc_overflow();
+        NetworkEndian::write_u32(&mut ts.writable()[..4], 167);
+        NetworkEndian::write_u32(&mut ts.writable()[4..8], 171);
+        ts.inc_readable_size(8);
+
+        assert_eq!(ts.flg(), 3);
+        assert_eq!(ts.overflow(), 1);
+        assert_eq!(ts.readable().len(), 8);
+        assert_eq!(ts.remaining(), 4);
+        assert_eq!(ts.writable().len(), 4);
+        assert_eq!(NetworkEndian::read_u32(&ts.readable()[..4]), 167);
+        assert_eq!(NetworkEndian::read_u32(&ts.readable()[4..8]), 171);
+
+        assert_eq!(rr.readable().len(), 0);
+        assert_eq!(rr.remaining(), 12);
+        assert_eq!(rr.writable().len(), 12);
+
+        (&mut rr.writable()[..4]).copy_from_slice(&[1, 2, 3, 4]);
+        rr.inc_readable_size(4);
+
+        assert_eq!(rr.readable().len(), 4);
+        assert_eq!(rr.remaining(), 8);
+        assert_eq!(rr.writable().len(), 8);
+        assert_eq!(&rr.readable()[..4], &[1, 2, 3, 4]);
+
+        ra.set_alert_value(566);
+        assert_eq!(ra.alert_value(), 566);
+
+        assert_eq!(writer.remaining_bytes(), 160);
+
+        let mut opt_iter = OptionIter::from_option_bytes(&buf[..40]);
+
+        if let Ipv4Option::Ts(ts) = opt_iter.next().unwrap() {
+            assert_eq!(ts.flg(), 3);
+            assert_eq!(ts.overflow(), 1);
+            assert_eq!(ts.readable().len(), 8);
+            assert_eq!(ts.remaining(), 4);
+            assert_eq!(NetworkEndian::read_u32(&ts.readable()[..4]), 167);
+            assert_eq!(NetworkEndian::read_u32(&ts.readable()[4..8]), 171);
+        } else {
+            assert!(false);
+        }
+
+        if let Ipv4Option::RR(rr) = opt_iter.next().unwrap() {
+            assert_eq!(rr.readable().len(), 4);
+            assert_eq!(rr.remaining(), 8);
+            assert_eq!(&rr.readable()[..4], &[1, 2, 3, 4]);
+        } else {
+            assert!(false);
+        }
+
+        if let Ipv4Option::Nop = opt_iter.next().unwrap() {
+            assert!(true);
+        } else {
+            assert!(false);
+        }
+
+        if let Ipv4Option::RA(ra) = opt_iter.next().unwrap() {
+            assert_eq!(ra.alert_value(), 566);
+        } else {
+            assert!(false);
+        }
+
+        if let Ipv4Option::Nop = opt_iter.next().unwrap() {
+            assert!(true);
+        } else {
+            assert!(false);
+        }
+
+        if let Ipv4Option::Nop = opt_iter.next().unwrap() {
+            assert!(true);
+        } else {
+            assert!(false);
+        }
+
+        if let Ipv4Option::Nop = opt_iter.next().unwrap() {
+            assert!(true);
+        } else {
+            assert!(false);
+        }
+
+        if let Ipv4Option::Eol = opt_iter.next().unwrap() {
+            assert!(true);
+        } else {
+            assert!(false);
+        }
+
+        let mut opt_iter = OptionIterMut::from_option_bytes_mut(&mut buf[..40]);
+
+        if let Ipv4OptionMut::Ts(mut ts) = opt_iter.next().unwrap() {
+            NetworkEndian::write_u32(&mut ts.writable()[..], 256);
+            ts.inc_readable_size(4);
+        } else {
+            assert!(false);
+        }
+
+        if let Ipv4OptionMut::RR(mut rr) = opt_iter.next().unwrap() {
+            (&mut rr.writable()[..4]).copy_from_slice(&[2, 4, 6, 8]);
+            rr.inc_readable_size(4);
+        } else {
+            assert!(false);
+        }
+
+        opt_iter.next().unwrap();
+
+        if let Ipv4OptionMut::RA(mut ra) = opt_iter.next().unwrap() {
+            ra.set_alert_value(820);
+        } else {
+            assert!(false);
+        }
+
+        let mut opt_iter = OptionIterMut::from_option_bytes_mut(&mut buf[..40]);
+
+        if let Ipv4OptionMut::Ts(ts) = opt_iter.next().unwrap() {
+            assert_eq!(ts.flg(), 3);
+            assert_eq!(ts.overflow(), 1);
+            assert_eq!(ts.readable().len(), 12);
+            assert_eq!(ts.remaining(), 0);
+            assert_eq!(NetworkEndian::read_u32(&ts.readable()[..4]), 167);
+            assert_eq!(NetworkEndian::read_u32(&ts.readable()[4..8]), 171);
+            assert_eq!(NetworkEndian::read_u32(&ts.readable()[8..12]), 256);
+        } else {
+            assert!(false);
+        }
+
+        if let Ipv4OptionMut::RR(rr) = opt_iter.next().unwrap() {
+            assert_eq!(rr.readable().len(), 8);
+            assert_eq!(rr.remaining(), 4);
+            assert_eq!(&rr.readable()[..4], &[1, 2, 3, 4]);
+            assert_eq!(&rr.readable()[4..8], &[2, 4, 6, 8]);
+        } else {
+            assert!(false);
+        }
+
+        if let Ipv4OptionMut::Nop = opt_iter.next().unwrap() {
+            assert!(true);
+        } else {
+            assert!(false);
+        }
+
+        if let Ipv4OptionMut::RA(ra) = opt_iter.next().unwrap() {
+            assert_eq!(ra.alert_value(), 820);
+        } else {
+            assert!(false);
+        }
+
+        if let Ipv4OptionMut::Nop = opt_iter.next().unwrap() {
+            assert!(true);
+        } else {
+            assert!(false);
+        }
+
+        if let Ipv4OptionMut::Nop = opt_iter.next().unwrap() {
+            assert!(true);
+        } else {
+            assert!(false);
+        }
+
+        if let Ipv4OptionMut::Nop = opt_iter.next().unwrap() {
+            assert!(true);
+        } else {
+            assert!(false);
+        }
+
+        if let Ipv4OptionMut::Eol = opt_iter.next().unwrap() {
+            assert!(true);
+        } else {
+            assert!(false);
         }
     }
 }
