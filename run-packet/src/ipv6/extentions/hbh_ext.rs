e6d95957bba71d4d869b47c732172706e68c107e
duanjp8617 <duanjp8617@gmail.com>
2023-11-14 12:53:45 +0000

    Update ipv6 extentions
    Now we make sure that headers have fixed length, while packet can be
    variable length

--- run-packet/src/ipv6/extentions/hbh_ext.rs
+++ run-packet/src/ipv6/extentions/hbh_ext.rs
@@ -4,73 +4,6 @@ use crate::ipv4::IpProtocol;
 use crate::PktMut;
 use crate::{Cursor, CursorMut};
 
-/// RFC2460 - Sec. 4.3
-#[derive(Clone, Copy, Debug)]
-pub struct Ipv6HbhExtHeader<T> {
-    buf: T,
-}
-
-impl<T: AsRef<[u8]>> Ipv6HbhExtHeader<T> {
-    #[inline]
-    pub fn new(buf: T) -> Result<Self, T> {
-        if buf.as_ref().len() >= 2 {
-            let header_len = usize::from(buf.as_ref()[1]) * 8 + 8;
-            if buf.as_ref().len() >= header_len {
-                Ok(Self { buf })
-            } else {
-                Err(buf)
-            }
-        } else {
-            Err(buf)
-        }
-    }
-
-    #[inline]
-    pub fn new_unchecked(buf: T) -> Self {
-        Self { buf }
-    }
-
-    #[inline]
-    pub fn as_bytes(&self) -> &[u8] {
-        let header_len = usize::from(self.buf.as_ref()[1]) * 8 + 8;
-        &self.buf.as_ref()[0..header_len]
-    }
-
-    #[inline]
-    pub fn next_header(&self) -> IpProtocol {
-        self.buf.as_ref()[0].into()
-    }
-
-    #[inline]
-    pub fn header_len(&self) -> usize {
-        usize::from(self.buf.as_ref()[1]) * 8 + 8
-    }
-
-    #[inline]
-    pub fn option_bytes(&self) -> &[u8] {
-        &self.buf.as_ref()[2..self.header_len()]
-    }
-}
-
-impl<T: AsMut<[u8]>> Ipv6HbhExtHeader<T> {
-    #[inline]
-    pub fn set_next_header(&mut self, value: IpProtocol) {
-        self.buf.as_mut()[0] = value.into();
-    }
-
-    #[inline]
-    pub fn set_header_len(&mut self, value: usize) {
-        assert!(value >= 8 && value <= 2048 && value % 8 == 0);
-        self.buf.as_mut()[1] = ((value - 8) / 8) as u8;
-    }
-
-    #[inline]
-    pub fn option_bytes_mut(&mut self) -> &mut [u8] {
-        let header_len = usize::from(self.buf.as_mut()[1]) * 8 + 8;
-        &mut self.buf.as_mut()[2..header_len]
-    }
-}
-
 #[derive(Debug)]
 #[repr(transparent)]
 pub struct Ipv6HbhExtPacket<T> {
@@ -82,18 +15,15 @@ impl<T: Buf> Ipv6HbhExtPacket<T> {
     pub fn parse_unchecked(buf: T) -> Self {
         Self { buf }
     }
+
     #[inline]
     pub fn buf(&self) -> &T {
         &self.buf
     }
+
     #[inline]
     pub fn release(self) -> T {
         self.buf
-    }
-    #[inline]
-    pub fn header(&self) -> Ipv6HbhExtHeader<&[u8]> {
-        let header_len = usize::from(self.buf.chunk()[1]) * 8 + 8;
-        Ipv6HbhExtHeader::new_unchecked(&self.buf.chunk()[..header_len])
     }
 
     #[inline]
@@ -154,17 +84,16 @@ impl<T: PktMut> Ipv6HbhExtPacket<T> {
     }
 
     #[inline]
-    pub fn prepend_header<HT: AsRef<[u8]>>(
-        mut buf: T,
-        header: &Ipv6HbhExtHeader<HT>,
-    ) -> Ipv6HbhExtPacket<T> {
-        let header_len = header.header_len();
+    pub fn prepend_header<HT: AsRef<[u8]>>(mut buf: T, header_len: usize) -> Ipv6HbhExtPacket<T> {
+        assert!(header_len >= 8 && header_len <= 2048 && header_len % 8 == 0);
 
         assert!(buf.chunk_headroom() >= header_len);
         buf.move_back(header_len);
 
         let data = &mut buf.chunk_mut()[0..header_len];
-        data.copy_from_slice(header.as_bytes());
+        (&mut data[2..]).fill(0);
+        data[0] = IpProtocol::TCP.into();
+        data[1] = ((header_len - 8) / 8) as u8;
 
         Ipv6HbhExtPacket { buf }
     }
@@ -172,10 +101,9 @@ impl<T: PktMut> Ipv6HbhExtPacket<T> {
 
 impl<'a> Ipv6HbhExtPacket<Cursor<'a>> {
     #[inline]
-    pub fn cursor_header(&self) -> Ipv6HbhExtHeader<&'a [u8]> {
+    pub fn cursor_option_bytes(&self) -> &'a [u8] {
         let header_len = self.header_len();
-        let data = &self.buf.chunk_shared_lifetime()[..header_len];
-        Ipv6HbhExtHeader::new_unchecked(data)
+        &self.buf.chunk_shared_lifetime()[2..header_len]
     }
 
     #[inline]
@@ -187,13 +115,10 @@ impl<'a> Ipv6HbhExtPacket<Cursor<'a>> {
 
 impl<'a> Ipv6HbhExtPacket<CursorMut<'a>> {
     #[inline]
-    pub fn split(self) -> (Ipv6HbhExtHeader<&'a mut [u8]>, CursorMut<'a>) {
+    pub fn split(self) -> (&'a mut [u8], CursorMut<'a>) {
         let header_len = self.header_len();
         let buf_mut = self.buf.chunk_mut_shared_lifetime();
         let (hdr, payload) = buf_mut.split_at_mut(header_len);
-        (
-            Ipv6HbhExtHeader::new_unchecked(hdr),
-            CursorMut::new(payload),
-        )
+        (&mut hdr[2..], CursorMut::new(payload))
     }
 }
