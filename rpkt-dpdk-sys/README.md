61df84a47307362baefbcb6955c382d38033f2b0
duanjp8617 <duanjp8617@gmail.com>
2025-03-19 17:58:18 +0800

    remove rpkt-dpdk-sys

--- rpkt-dpdk-sys/README.md
+++
@@ -1,7 +0,0 @@
-# DPDK Version
-Minimal supported DPDK version is 21.11.
-
-Tested on 23.11
-
-It is not guaranteed to work on anything above 24.03 though.
-
--- rpkt-dpdk-sys/src/lib.rs
+++
@@ -1,5 +0,0 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-#![allow(clippy::redundant_static_lifetimes)]
-include!(concat!(env!("OUT_DIR"), "/dpdk.rs"));
