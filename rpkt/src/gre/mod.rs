fc04c84ea3b0616aa0507dbfc891d2952c998084
duanjp8617 <duanjp8617@gmail.com>
2025-07-03 19:22:47 +0800

    add helper functions for calculating gre header length

--- rpkt/src/gre/mod.rs
+++ rpkt/src/gre/mod.rs
@@ -3,20 +3,10 @@ use crate::ether::EtherType;
 use crate::traits::*;
 
 mod generated;
-pub use generated::{GREBASE_HEADER_LEN, GREBASE_HEADER_TEMPLATE};
+pub use generated::*;
 
-// From left to right, we have 16 bits:
-// checksum (1)
-// routing (1)
-// key (1)
-// sequence number (1)
-// strict source route (1)
-// recursion control (3)
-// ack (1)
-// flags (4)
-// verison (3)
 #[inline]
-fn gre_fixed_header_len(indicator_field: u16) -> usize {
+fn gre_header_len(indicator_field: u16) -> usize {
     let options = [
         // checksum
         ((indicator_field & (1 << 15) != 0) | (indicator_field & (1 << 14) != 0)),
@@ -24,11 +14,26 @@ fn gre_fixed_header_len(indicator_field: u16) -> usize {
         indicator_field & (1 << 13) != 0,
         // seq
         indicator_field & (1 << 12) != 0,
+    ];
+
+    options.iter().fold(4, |mut aggre, item| {
+        if *item {
+            aggre += 4;
+        }
+        aggre
+    })
+}
+
+#[inline]
+fn gre_pptp_header_len(indicator_field: u16) -> usize {
+    let options = [
+        // seq
+        indicator_field & (1 << 12) != 0,
         // ack
         indicator_field & (1 << 7) != 0,
     ];
 
-    4 + options.iter().fold(0, |mut aggre, item| {
+    options.iter().fold(8, |mut aggre, item| {
         if *item {
             aggre += 4;
         }
