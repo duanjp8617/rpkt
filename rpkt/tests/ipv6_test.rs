6a96966c4563807f9410971e2c8056781c63c08c
duanjp8617 <duanjp8617@gmail.com>
2025-08-25 17:39:17 +0800

    test: Add IPv6 destination options packet build test
      Implement ipv6_options_destination_build() function that constructs an IPv6 packet with
      destination options extension header containing Generic (type 11) and PadN options, then
       compares the built packet with the reference data file.

--- rpkt/tests/ipv6_test.rs
+++ rpkt/tests/ipv6_test.rs
@@ -74,3 +74,54 @@ fn ipv6_options_destination_parse() {
     let payload = udp_pkt.payload();
     assert_eq!(payload.chunk().len(), 10);
 }
+
+#[test]
+fn ipv6_options_destination_build() {
+    let pkt = file_to_packet("ipv6_options_destination.dat");
+    let mut buf = [0; 1600];
+    let mut pbuf = CursorMut::new(&mut buf);
+    pbuf.advance(1600);
+
+    pbuf.move_back(10);
+    pbuf.chunk_mut().copy_from_slice(&pkt[pkt.len() - 10..]);
+
+    let mut udp = Udp::prepend_header(pbuf, &UDP_HEADER_TEMPLATE);
+    udp.set_src_port(42513);
+    udp.set_dst_port(42);    
+    udp.set_checksum(0x6889);
+
+    let mut hdr = DestOptions::default_header();
+    DestOptions::from_header_array_mut(&mut hdr).set_header_len(8);
+    let mut dest_opts = DestOptions::prepend_header(udp.release(), &hdr);
+    dest_opts.set_next_header(IpProtocol::UDP);
+
+    let mut option_pbuf = CursorMut::new(dest_opts.var_header_slice_mut());
+    option_pbuf.advance(6);
+
+    let mut padn_hdr = Padn::default_header();
+    Padn::from_header_array_mut(&mut padn_hdr).set_header_len(3);
+    let mut padn_opt = Padn::prepend_header(option_pbuf, &padn_hdr);
+    padn_opt.var_header_slice_mut()[0] = 0x00;
+
+    let mut generic_hdr = Generic::default_header();
+    Generic::from_header_array_mut(&mut generic_hdr).set_header_len(3);
+    let mut generic_opt = Generic::prepend_header(padn_opt.release(), &generic_hdr);
+    generic_opt.set_type_(11);
+    generic_opt.var_header_slice_mut()[0] = 0x09;
+
+    let mut ipv6 = Ipv6::prepend_header(dest_opts.release(), &IPV6_HEADER_TEMPLATE);
+    ipv6.set_version(6);
+    ipv6.set_traffic_class(0);
+    ipv6.set_flow_label(0);    
+    ipv6.set_next_header(IpProtocol::IPV6_OPTS);
+    ipv6.set_hop_limit(64);
+    ipv6.set_src_addr(Ipv6Addr::from_str("2a01:e35:8bd9:8bb0:a0a7:ea9c:74e8:d397").unwrap());
+    ipv6.set_dst_addr(Ipv6Addr::from_str("2001:4b98:dc0:41:216:3eff:fece:1902").unwrap());
+
+    let mut eth = EtherFrame::prepend_header(ipv6.release(), &ETHER_FRAME_HEADER_TEMPLATE);
+    eth.set_dst_addr(EtherAddr([0xf4, 0xca, 0xe5, 0x4d, 0x1f, 0x41]));
+    eth.set_src_addr(EtherAddr([0x00, 0x1e, 0x8c, 0x76, 0x29, 0xb6]));
+    eth.set_ethertype(EtherType::IPV6);
+
+    assert_eq!(eth.release().chunk(), &pkt);
+}
