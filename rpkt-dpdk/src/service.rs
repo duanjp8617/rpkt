e59896d443156e883ef7ae422a90da3af570c846
duanjp8617 <duanjp8617@gmail.com>
2025-09-03 12:02:10 +0000

    update service

--- rpkt-dpdk/src/service.rs
+++ rpkt-dpdk/src/service.rs
@@ -39,7 +39,7 @@ impl DpdkOption {
     ///
     /// # Examples
     ///
-    /// ```
+    /// ```rust
     /// use rpkt_dpdk::DpdkOption;
     ///
     /// let res = DpdkOption::with_eal_arg("-l 2 -n 4 --file-prefix app1").init();
@@ -53,11 +53,12 @@ impl DpdkOption {
 
     /// Initialize dpdk eal using the provided eal argument string.
     ///
-    /// After initialization, the global singleton [`DpdkService`] can be accessed.
+    /// After initialization, the global singleton [`DpdkService`] can be
+    /// accessed.
     ///
     /// # Examples
     ///
-    /// ```
+    /// ```rust
     /// use rpkt_dpdk::DpdkOption;
     ///
     /// let res = DpdkOption::default().init();
@@ -114,9 +115,9 @@ struct ServiceInner {
 }
 
 /// A global singleton providing all the dpdk services.
-/// 
-/// The provided dpdk services include: 
-/// - 
+///
+/// The provided dpdk services include:
+/// -
 pub struct DpdkService {
     service: Mutex<ServiceInner>,
     lcores: Vec<Lcore>,
@@ -136,7 +137,7 @@ pub fn try_service() -> Result<&'static DpdkService> {
 /// Return a reference to the [`DpdkService`].
 ///
 /// # Panics
-/// 
+///
 /// This function panics if  [`DpdkService`] is not correctly initialized.
 pub fn service() -> &'static DpdkService {
     match SERVICE.get() {
@@ -146,25 +147,40 @@ pub fn service() -> &'static DpdkService {
 }
 
 impl DpdkService {
-    /// Return a static reference to a list of `Lcore` on the current machine.
+    /// Get a list of [`Lcore`] on the current machine.
     ///
-    /// Each `Lcore` contains the lcore id, cpu id and the socket id of the corresponding
-    /// physical CPU core.
+    /// The lcore list is collected by analyzing the /sys directory of the linux
+    /// file system.
     ///
-    /// We obtain the list of `Lcore` by reading the `/sys/` folder under the
-    /// Linux file system.
+    /// The returned lcore list is sorted by the lcore id in ascending order.
+    ///
+    /// # Example
+    /// ```rust
+    /// use rpkt_dpdk::{service, DpdkOption};
+    ///
+    /// DpdkOption::with_eal_arg("-l 2 -n 4 --file-prefix app1")
+    ///     .init()
+    ///     .unwrap();
+    /// let sorted = service()
+    ///     .lcores()
+    ///     .iter()
+    ///     .map(|lcore| lcore.lcore_id)
+    ///     .is_sorted();
+    /// assert_eq!(sorted, true);
+    /// ```
     pub fn lcores(&self) -> &Vec<Lcore> {
         &self.lcores
     }
 
     /// Bind the current thread to the lcore indicated by `lcore_id`.
     ///
-    /// This function will fail and return an `Err` if the following conditions happen:
+    /// This function will fail and return an `Err` if the following conditions
+    /// happen:
     /// 1. The DPDK service has been shutdown.
     /// 2. The lcore id is invalid on the current machine.
     /// 3. The thread has already been bind to an lcore.
     /// 4. The lcore has already been bind to another thread.
-    /// 5. The DPDK FFI fails with lcore binding.    
+    /// 5. The DPDK FFI fails with lcore binding.
     pub fn lcore_bind(&self, lcore_id: u32) -> Result<()> {
         let mut inner = self.try_lock()?;
 
@@ -201,8 +217,8 @@ impl DpdkService {
         if !mp.in_use() && mp.full() {
             // We are the sole owner of the counter, this also means that
             // we are the sole owner of the PtrWrapper, and we are safe to deallocate it.
-            // The mempool to be removed is also full, this means that there are no out-going
-            // mbuf pointers.
+            // The mempool to be removed is also full, this means that there are no
+            // out-going mbuf pointers.
             let mp = inner.mpools.remove(name).unwrap();
             unsafe { Mempool::delete(mp) }
             Ok(())
