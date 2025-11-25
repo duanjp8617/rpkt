0c248f67c1e61bb7b76d24474307510c6b184fae
duanjp8617 <duanjp8617@gmail.com>
2025-11-25 09:04:00 +0000

    add doc for port

--- rpkt-dpdk/src/port.rs
+++ rpkt-dpdk/src/port.rs
@@ -7,6 +7,8 @@ use crate::error::*;
 use crate::Mbuf;
 use crate::Mempool;
 
+// `Port` is internally maintained by `DpdkService` to track the liveness of
+// `RxQueue` and `TxQueue`
 pub(crate) struct Port {
     rxq_cts: Vec<(RxQueue, Mempool)>,
     txqs: Vec<TxQueue>,
@@ -32,6 +34,7 @@ impl Port {
             .get(usize::from(qid))
             .ok_or(DpdkError::service_err("invalid queue id"))?;
 
+        // rx queue can only be given out once.
         rxq_ct.0.clone_once()
     }
 
@@ -66,6 +69,7 @@ impl Port {
     }
 }
 
+/// The rx queue of a dpdk port.
 pub struct RxQueue {
     port_id: u16,
     qid: u16,
@@ -73,6 +77,18 @@ pub struct RxQueue {
 }
 
 impl RxQueue {
+    /// Receive a batch of packets from the rx queue.
+    ///
+    /// Dpdk stores packet data on a special data structure [`Mbuf`] and relies
+    /// heavily on batched processing to accelerate performance. Instead of
+    /// receiving a single [`Mbuf`] for each time, dpdk tries to receive a batch
+    /// of [`Mbuf`]s (usually 32).
+    ///
+    /// The `rx` method is a safe wrapper for the dpdk receiving API
+    /// [`ffi::rte_eth_rx_burst_`]. Here, the input argument `batch` will store
+    /// the received [`Mbuf`] in the non-occupied area of [`ArrayVec`]. The
+    /// return value indicates the total number of the received packets,
+    /// with maximum value being `N-batch.len()`.
     #[inline]
     pub fn rx<const N: usize>(&mut self, batch: &mut ArrayVec<Mbuf, N>) -> usize {
         assert!(N <= usize::from(u16::MAX));
@@ -91,7 +107,6 @@ impl RxQueue {
         }
     }
 
-    // Safety: the mp must be a valid pointer throughout the lifetime of the RxQueue
     pub(crate) fn new(port_id: u16, qid: u16) -> Self {
         Self {
             port_id,
@@ -117,6 +132,7 @@ impl RxQueue {
     }
 }
 
+/// The tx queue of a dpdk port.
 pub struct TxQueue {
     port_id: u16,
     qid: u16,
@@ -124,6 +140,16 @@ pub struct TxQueue {
 }
 
 impl TxQueue {
+    /// Transmit a batch of packets out from the tx queue.
+    ///
+    /// `tx` also relies on batched processing and is the mirror method of `rx`.
+    ///
+    /// The `tx` method is a safe wrapper for the dpdk transmitting API
+    /// [`ffi::rte_eth_tx_burst_`]. Here, the input argument `batch` contains
+    /// the [`Mbuf`] to send.
+    ///  The return value indicates the actual number of [`Mbuf`]s being sent.
+    /// After `tx` returns, the length of `batch` becomes `N` minus `tx`'s
+    /// return value.
     #[inline]
     pub fn tx<const N: usize>(&mut self, batch: &mut ArrayVec<Mbuf, N>) -> usize {
         assert!(N <= usize::from(u16::MAX));
@@ -169,54 +195,68 @@ impl TxQueue {
     }
 }
 
+/// The dpdk port statistics.
 #[derive(Clone, Copy)]
 pub struct PortStats(ffi::rte_eth_stats);
 
 impl PortStats {
+    /// Total number of successfully received packets.
     pub fn ipackets(&self) -> u64 {
         self.0.ipackets
     }
 
+    /// Total number of successfully transmitted packets.
     pub fn opackets(&self) -> u64 {
         self.0.opackets
     }
 
+    /// Total number of successfully received bytes.
     pub fn ibytes(&self) -> u64 {
         self.0.ibytes
     }
 
+    /// Total number of successfully transmitted bytes.
     pub fn obytes(&self) -> u64 {
         self.0.obytes
     }
 
+    /// Total of Rx packets dropped by the HW, because there are no available
+    /// buffer (i.e. Rx queues are full).
     pub fn imissed(&self) -> u64 {
         self.0.imissed
     }
 
+    /// Total number of failed transmitted packets.
     pub fn oerrors(&self) -> u64 {
         self.0.oerrors
     }
 
+    /// Total number of Rx mbuf allocation failures.
     pub fn rx_nombuf(&self) -> u64 {
         self.0.rx_nombuf
     }
 
+    /// Total number of successfully received packets for queue `qid`.
     pub fn q_ipackets(&self, qid: usize) -> u64 {
         self.0.q_ipackets[qid]
     }
 
+    /// Total number of successfully transmitted packets for queue `qid`.
     pub fn q_opackets(&self, qid: usize) -> u64 {
         self.0.q_opackets[qid]
     }
 
+    /// Total number of successfully received bytes for queue `qid`.
     pub fn q_ibytes(&self, qid: usize) -> u64 {
         self.0.q_ibytes[qid]
     }
 
+    /// Total number of successfully transmitted bytes for queue `qid`.
     pub fn q_obytes(&self, qid: usize) -> u64 {
         self.0.q_obytes[qid]
     }
 
+    /// Total number of dropped packets for queue `qid`.
     pub fn q_errors(&self, qid: usize) -> u64 {
         self.0.q_errors[qid]
     }
@@ -229,14 +269,14 @@ impl Default for PortStats {
     }
 }
 
-/// A context to query the stats counters from the port.
-/// This context is reference counted.
+/// A context to query the `PortStats` from the dpdk port.
 pub struct StatsQuery {
     port_id: u16,
     counter: Arc<()>,
 }
 
 impl StatsQuery {
+    /// Query a new `PortStats`.
     pub fn query(&mut self) -> PortStats {
         unsafe {
             let mut port_stats: ffi::rte_eth_stats = std::mem::zeroed();
@@ -248,6 +288,7 @@ impl StatsQuery {
         }
     }
 
+    /// Update `port_stats` to the latest port stats.
     pub fn update(&mut self, port_stats: &mut PortStats) {
         unsafe {
             let res =
